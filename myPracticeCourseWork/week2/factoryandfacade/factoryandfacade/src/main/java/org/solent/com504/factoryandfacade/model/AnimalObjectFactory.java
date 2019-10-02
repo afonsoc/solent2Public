a95e4b09c73e5d2c48039c68e444c1558b700ce0
Afonso Marques Mendes Cabecadas <3marqa74@solent.ac.uk>
2019-10-02 12:43:30 +0100

    exercise farmfacade

---
+++ myPracticeCourseWork/week2/factoryandfacade/factoryandfacade/src/main/java/org/solent/com504/factoryandfacade/model/AnimalObjectFactory.java
@@ -0,0 +1,23 @@
+package org.solent.com504.factoryandfacade.model;
+
+import org.solent.com504.factoryandfacade.model.FarmFacadeImpl;
+
+public class AnimalObjectFactory{
+
+    public static Animal createCat() {
+        return new Cat();
+    }
+
+    public static Animal createDog() {
+        return new Dog();
+    }
+
+    public static Animal createCow() {
+        return new Cow();
+    }
+
+    public static FarmFacade createFarmFacade() {
+        FarmFacade farmfacade = new FarmFacadeImpl();
+        return new FarmFacadeImpl();
+    }
+}
---
+++ myPracticeCourseWork/week2/factoryandfacade/factoryandfacade/src/main/java/org/solent/com504/factoryandfacade/model/FarmFacadeImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.factoryandfacade.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author 3marqa74
+ */
+public class FarmFacadeImpl implements FarmFacade {
+
+    @Override
+    public List<Animal> getAllAnimals() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.        
+    }
+    
+
+    @Override
+    public void addDog(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    @Override
+    public void addCat(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addCow(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
